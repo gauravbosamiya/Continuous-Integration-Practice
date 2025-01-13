11a99987c3f279851b7970866b9ebdd583c3f935
Gaurav <gauravbosamiya9@gmail.com>
2025-01-13 19:33:10 +0530

    _test.py updated

--- _test.py
+++ _test.py
@@ -11,17 +11,17 @@ def fifth_power(n):
     return n**5
 
 def test_square():
-    assert square(2) == 4, "test Failed: Square of 2 should be 4"
-    assert square(3) == 9, "test Failed: Square of 3 should be 9"
+    assert square(2) == 4, "Test Failed: Square of 2 should be 4"
+    assert square(3) == 9, "Test Failed: Square of 3 should be 9"
     
     
 def test_cube():
-    assert cube(2) == 8, "test Failed: Square of 2 should be 8"
-    assert cube(3) == 27, "test Failed: Square of 3 should be 27"
+    assert cube(2) == 8, "Test Failed: Cube of 2 should be 8"
+    assert cube(3) == 27, "Test Failed: Cube of 3 should be 27"
     
 def test_fifth_power():
-    assert fifth_power(2) == 32, "test Failed: Square of 2 should be 32"
-    assert fifth_power(3) == 243, "test Failed: Square of 3 should be 243"
+    assert fifth_power(2) == 32, "Test Failed: Fifth power of 2 should be 32"
+    assert fifth_power(3) == 243, "Test Failed: Fifth power of 3 should be 243"
     
 def test_invalid_input():
     with pytest.raises("TypeError"):
