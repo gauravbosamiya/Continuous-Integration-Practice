10e8c65954ce54df61ece62baa04b9c95a1aced8
Gaurav <gauravbosamiya9@gmail.com>
2025-01-13 17:37:34 +0530

    CI Implemented

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st 
 
 st.title("Power Calculator")
-st.write("Enter a number to calculate its power till that number")
+st.write("Enter a number to calculate square, cub, and fifth power")
 
 n = st.number_input("Ente a integer", value=1, step=1)
 
-l= []
-for i in range(n):
-    l.append(n ** i)
-    
-st.write(l)
+square = n**2
+cube = n**3
+fifth = n**5
+
+st.write(f"square of {n} is", square)
+st.write(f"cube of {n} is", cube)
+st.write(f"fifth power of {n} is", fifth)
